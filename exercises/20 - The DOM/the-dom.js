f9eda02a561f7313c26e9ca2778e65a9b0561288
Daynya Quigley <daynya@gmail.com>
2020-03-30 09:30:35 -0400

    Lesson 22.

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,7 @@
+const p = document.querySelector('p');
+const imgs = document.querySelectorAll('.item img');
+const item2 = document.querySelector('.item2');
+const item2image = item2.querySelector('img');
+
+console.log(p);
+console.log(item2image);
