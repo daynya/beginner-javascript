c99a9a4e8d34a9f2dc3086f769c966f26c0227bc
Daynya Quigley <daynya@gmail.com>
2020-03-31 09:35:17 -0400

    Lesson 22.

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -2,6 +2,16 @@ const p = document.querySelector('p');
 const imgs = document.querySelectorAll('.item img');
 const item2 = document.querySelector('.item2');
 const item2image = item2.querySelector('img');
+const heading = document.querySelector('h2');
 
-console.log(p);
-console.log(item2image);
+console.log(heading.textContent);
+// heading.textContent = "updated text!";
+// console.log(heading.textContent);
+// console.log(heading.innerText);
+
+console.log(heading.innerHTML);
+
+const pizzaList = document.querySelector('.pizza');
+console.log(pizzaList.textContent);
+//pizzaList.textContent = `${pizzaList.textContent} 🍕`; 
+pizzaList.insertAdjacentText('beforeend', '🍕');
