91aa22d9195229d367bdb9200265ca1a48d3bd3c
Daynya Quigley <daynya@gmail.com>
2020-04-09 08:38:30 -0400

    Lesson 23

--- exercises/20 - The DOM/the-dom.js
+++ exercises/20 - The DOM/the-dom.js
@@ -1,17 +1,29 @@
-const p = document.querySelector('p');
-const imgs = document.querySelectorAll('.item img');
-const item2 = document.querySelector('.item2');
-const item2image = item2.querySelector('img');
-const heading = document.querySelector('h2');
+// const p = document.querySelector('p');
+// const imgs = document.querySelectorAll('.item img');
+// const item2 = document.querySelector('.item2');
+// const item2image = item2.querySelector('img');
+// const heading = document.querySelector('h2');
 
-console.log(heading.textContent);
-// heading.textContent = "updated text!";
 // console.log(heading.textContent);
-// console.log(heading.innerText);
+// // heading.textContent = "updated text!";
+// // console.log(heading.textContent);
+// // console.log(heading.innerText);
 
-console.log(heading.innerHTML);
+// console.log(heading.innerHTML);
 
-const pizzaList = document.querySelector('.pizza');
-console.log(pizzaList.textContent);
-//pizzaList.textContent = `${pizzaList.textContent} 🍕`; 
-pizzaList.insertAdjacentText('beforeend', '🍕');
+// const pizzaList = document.querySelector('.pizza');
+// console.log(pizzaList.textContent);
+// //pizzaList.textContent = `${pizzaList.textContent} 🍕`; 
+// pizzaList.insertAdjacentText('beforeend', '🍕')
+
+// Classes!
+const pic = document.querySelector('.nice');
+pic.classList.add('open');
+pic.classList.remove('cool');
+console.log(pic.classList);
+
+function toggleRound() {
+    pic.classList.toggle('round');
+}
+
+pic.addEventListener('click', toggleRound);
