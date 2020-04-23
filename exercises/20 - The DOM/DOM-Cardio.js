7b20ab2dafc424b5fe562b83a5abc1a4555c9056
Daynya Quigley <daynya@gmail.com>
2020-04-23 15:59:50 -0400

    Lesson 28

--- exercises/20 - The DOM/DOM-Cardio.js
+++ exercises/20 - The DOM/DOM-Cardio.js
@@ -1,21 +1,42 @@
 // Make a div
+const myDiv = document.createElement('div');
 
 // add a class of wrapper to it
+myDiv.classList.add('wrapper');
 
 // put it into the body
+document.body.appendChild(myDiv);
 
 // make an unordered list
+const myList = document.createElement('ul');
 
 // add three list items with the words "one, two three" in them
+const item = document.createElement('li');
+item.textContent = 'one';
+myList.appendChild(item);
+const item2 = document.createElement('li');
+item2.textContent = 'two';
+myList.insertAdjacentElement('afterbegin', item);
+const item3 = document.createElement('li');
+item3.textContent = 'three';
+myList.insertAdjacentElement('afterbegin', item2);
+
 // put that list into the above wrapper
+myDiv.insertAdjacentText('afterbegin', myList);
 
 // create an image
-
+const image = document.createElement('img');
 // set the source to an image
+image.src = 'https://picsum.photos/250';
+
 // set the width to 250
 // add a class of cute
+image.classList.add('cute');
+
 // add an alt of Cute Puppy
+image.alt = 'Cute Puppy';
 // Append that image to the wrapper
+myDiv.append(image);
 
 // with HTML string, make a div, with two paragraphs inside of it
 // put this div before the unordered list from above
