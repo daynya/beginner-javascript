b329860434b72e1fef829d7b8aeb977b38822b5d
Daynya Quigley <daynya@gmail.com>
2020-06-02 09:49:28 -0400

    33 Etch a Sketch

--- exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -7,20 +7,33 @@ const shakebutton = document.querySelector('.shake');
 // Make a variable called height and width from the same properties on our canvas
 const { width, height } = canvas;
 
+// Create random x and y start points on canvas
+const x = Math.floor(Math.random() * width); 
+const y = Math.floor(Math.random() * height);
+
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 10;
 
 ctx.beginPath(); // start drawing
-ctx.moveTo(200, 200);
-ctx.lineTo(200, 200);
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
 ctx.stroke();
 
 // Write a draw function
 
 
 // Write handler for keys
+function handleKey(e) {
+    if (e.key.includes('Arrow')) {
+        e.preventDefault();
+        console.log(e.key);
+        console.log("handling key");
+    }
+
+}
 
 // Clear / shake function
 
 // Listen for arrow keys
+window.addEventListener('keydown', handleKey);
