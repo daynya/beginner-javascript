2e7ff4ccffe6d816cd7a8a492774910e5cb2fded
Daynya Quigley <daynya@gmail.com>
2020-06-04 15:36:36 -0400

    Lesson 33, more work.

--- exercises/33 - Etch-a-Sketch/etch-a-sketch.js
+++ exercises/33 - Etch-a-Sketch/etch-a-sketch.js
@@ -2,18 +2,19 @@
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake');
+const MOVE_AMOUNT = 10;
 
 // Setup canvas for drawing
 // Make a variable called height and width from the same properties on our canvas
 const { width, height } = canvas;
 
 // Create random x and y start points on canvas
-const x = Math.floor(Math.random() * width); 
-const y = Math.floor(Math.random() * height);
+let x = Math.floor(Math.random() * width); 
+let y = Math.floor(Math.random() * height);
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 10;
+ctx.lineWidth = MOVE_AMOUNT;
 
 ctx.beginPath(); // start drawing
 ctx.moveTo(x, y);
@@ -21,16 +22,38 @@ ctx.lineTo(x, y);
 ctx.stroke();
 
 // Write a draw function
-
+function draw({ key }) {
+    console.log(key);
+    // start path
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    // move x and y values depending on what user did
+    switch (key) {
+        case 'ArrowUp':
+            y -= MOVE_AMOUNT;
+            break;
+        case 'ArrowRight':
+            x += MOVE_AMOUNT;
+        break;
+        case 'ArrowDown':
+            y += MOVE_AMOUNT;
+            break;
+        case 'ArrowLeft':
+            x -= MOVE_AMOUNT;
+            break;
+        default: 
+            break;
+    }
+    ctx.lineTo(x,y);
+    ctx.stroke();
+}
 
 // Write handler for keys
 function handleKey(e) {
     if (e.key.includes('Arrow')) {
         e.preventDefault();
-        console.log(e.key);
-        console.log("handling key");
+        draw({ key: e.key });
     }
-
 }
 
 // Clear / shake function
