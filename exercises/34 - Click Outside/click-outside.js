5d9660d86421c7429e7672416570a18107bceb17
Daynya Quigley <daynya@gmail.com>
2020-06-09 09:20:26 -0400

    Finished click outside modal.

--- exercises/34 - Click Outside/click-outside.js
+++ exercises/34 - Click Outside/click-outside.js
@@ -1,4 +1,5 @@
 const cardButtons = document.querySelectorAll('.card button');
+const modalOuter = document.querySelector('.modal-outer');
 const modalInner = document.querySelector('.modal-inner');
 
 function handleCardButtonClick(event) {
@@ -6,9 +7,26 @@ function handleCardButtonClick(event) {
     const card = button.closest('.card');
     const imgSrc = card.querySelector('img').src;
     const desc = card.dataset.description;
-    modalInner.innerHTML = ` img src`
-    console.log(desc);
+    const name = card.querySelector('h2').textContent;
+    modalInner.innerHTML = `
+        <img width="600" height="600" src ="${imgSrc.replace('200', '600')}", alt="${name}"/>
+        <p>${desc}</p>
+    `;
+    //show modal
+    modalOuter.classList.add('open');
 }
 
 cardButtons.forEach(button => button.addEventListener('click', handleCardButtonClick));
 
+function closeModal() {
+    modalOuter.classList.remove('open');
+}
+
+modalOuter.addEventListener('click', function(event) {
+    const isOutside = !event.target.closest('.modal-inner');
+    if (isOutside) {
+        closeModal();
+    }
+});
+
+window.addEventListener('keydown', (event) => { if(event.key === 'Escape') { closeModal(); }});
