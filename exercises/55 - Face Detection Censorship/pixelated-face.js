65e4f29dbcdcf66ac85fad58f7b6924bea45a5c7
Daynya Quigley <daynya@gmail.com>
2020-07-29 08:46:18 -0400

    Face detection.

--- exercises/55 - Face Detection Censorship/pixelated-face.js
+++ exercises/55 - Face Detection Censorship/pixelated-face.js
@@ -4,10 +4,11 @@ const canvas = document.querySelector('.video');
 const ctx = canvas.getContext('2d');
 
 const faceCanvas = document.querySelector('.face');
-const faceCtx = canvas.getContext('2d');
+const faceCtx = faceCanvas.getContext('2d');
 
 const faceDetector = new window.FaceDetector();
-console.log(video, canvas, faceCanvas, faceDetector);
+const SIZE = 10;
+const SCALE = 2;
 
 // write a function to populate the users video
 
@@ -26,9 +27,9 @@ async function populateVideo() {
 
 async function detect() {
   const faces = await faceDetector.detect(video);
-  console.log(faces.length);
   // ask browser when the next animation frame is, tell it to run detect for us
   faces.forEach(drawFace);
+  faces.forEach(censor);
   requestAnimationFrame(detect);
 }
 
@@ -38,6 +39,41 @@ function drawFace(face) {
   ctx.strokeStyle = '#ffc600';
   ctx.lineWidth = 2;
   ctx.strokeRect(left, top, width, height);
+}
+
+function censor( { boundingBox:face }) {
+  faceCtx.imageSmoothingEnabled = false;
+  faceCtx.clearRect(0,0,faceCanvas.width, faceCavnas.height);
+  // draw small face
+  faceCtx.drawImage(
+      // 5 source args
+      video, // where does the source come from
+      face.x, // where do we start the source pull from
+      face.y, 
+      face.width, 
+      face.height,
+      // 4 draw args
+      face.x, // where should we start drawing the x and y? 
+      face.y, 
+      SIZE, 
+      SIZE
+    );
+
+  // take face back out and draw it back at normal size
+  const width = face.width * SCALE;
+  const height = face.height * SCALE;
+  faceCtx.drawImage(
+    faceCanvas, // source
+    face.x, 
+    face.y,
+    SIZE,
+    SIZE,
+    // drawing args
+    face.x - (width - face.width) / 2, 
+    face.y - (height - face.height) / 2,
+    width, 
+    height
+  );
 }
 
 populateVideo().then(detect);
