8df57328be77f1ac8ca8a87e8c767acda11bd566
Daynya Quigley <daynya@gmail.com>
2020-08-07 09:06:31 -0400

    Slider

--- exercises/59 - Slider/index.js
+++ exercises/59 - Slider/index.js
@@ -8,7 +8,16 @@ function Slider(slider) {
     let next;
     // select elements needed for the slider
     const slides = slider.querySelector('.slides');
-    console.log(slides);
+    const prevButton = document.querySelector('.goToPrev');
+    const nextButton = document.querySelector('.goToNext');
+
+    function startSlider() {
+        current = slider.querySelector('.current') || slides.firstElementChild;
+        console.log(current);
+    }
+
+    //when this slider is created, run the start slider function
+    startSlider();
 
 }
 
