09ab7858c591b6b8c3f1c33e0719716b305135b6
Daynya Quigley <daynya@gmail.com>
2020-09-11 08:33:21 -0400

    Finished Async Typer UI

--- exercises/73 - Async Typer/scripts.js
+++ exercises/73 - Async Typer/scripts.js
@@ -6,16 +6,38 @@ function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()) {
     return Math.floor(randomNumber * (max - min) + min);
 }
 
-async function draw(el) {
+
+// USING FOR LOOP
+// async function draw(el) {
+//     const text = el.textContent;
+//     let soFar = '';
+//     for(const letter of text) {
+//         soFar += letter;
+//         el.textContent = soFar;
+//         const { typeMin, typeMax } = el.dataset;
+//         const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+//         await wait(amountOfTimeToWait);
+//     }
+// }
+
+// USING RECURSION
+
+function draw(el) {
+    let index = 1;
     const text = el.textContent;
-    let soFar = '';
-    for(const letter of text) {
-        console.log(letter);
-        soFar += letter;
-        el.textContent = soFar;
-        const amountOfTimeToWait = getRandomBetween();
+    const { typeMin, typeMax } = el.dataset;
+    async function drawLetter() {
+        el.textContent = text.slice(0, index);
+        index += 1;
+        // exit condition
+        const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
         await wait(amountOfTimeToWait);
+        if(index <= text.length) {
+            drawLetter();
+        }
     }
+    // when this function draw runs, kick off draw letter
+    drawLetter();
 }
 
 document.querySelectorAll('[data-type').forEach(draw);
