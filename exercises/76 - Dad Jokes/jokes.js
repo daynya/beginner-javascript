bf050b7c7766a8c044d13fd97774cc609621d7b3
Daynya Quigley <daynya@gmail.com>
2020-09-17 08:20:23 -0400

    Start of Dad Joke generator.

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -1,3 +1,6 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+
 const buttonText = [
   'Ugh.',
   '🤦🏻‍♂️',
@@ -8,3 +11,25 @@ const buttonText = [
   'please stop',
   'that was the worst one',
 ];
+
+async function fetchJoke() {
+  const response = await fetch('https://icanhazdadjoke.com', { 
+    headers: { 
+      Accept: 'application/json',
+    },
+  });
+  const data = await response.json();
+  return data;
+}
+
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+  return item;
+} 
+
+async function handleClick() {
+  const { joke } = await fetchJoke();
+  jokeHolder.textContent = joke;
+}
+
+jokeButton.addEventListener('click', handleClick);
