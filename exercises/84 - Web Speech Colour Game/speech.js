5833d1495966937e5671c972312d637ad97da7d2
Daynya Quigley <daynya@gmail.com>
2020-11-10 10:49:57 -0500

    Starting logic for Speech color game.

--- exercises/84 - Web Speech Colour Game/speech.js
+++ exercises/84 - Web Speech Colour Game/speech.js
@@ -1,9 +1,19 @@
 window.SpeechRecognition =
     window.SpeechRecognition ||
-    ßwindow.webkitSpeechRecognition;
+    window.webkitSpeechRecognition;
 
 function start() {
   // see if browser supports this
+  if(!('SpeechRecognition' in window)) {
+    console.log("Sorry your browser does not support speech recognition");
+    return;
+  }
+  // it does work
+  console.log('starting...');
+  // make new speech recognition
+  const recognition = new SpeechRecognition();
+  recognition.continuous = true;
+  recognition.interimResults = true;
 }
 
 start();
